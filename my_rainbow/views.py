073e0ebeaf8cb357392ad288a230a444b1987af9
miximc <miximc@mail.ru>
2023-02-02 15:39:34 +0300

    Радуга через Django

--- my_rainbow/views.py
+++ my_rainbow/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
+    
     return render(
-        request,               
+        request,
         'my_rainbow/index.html',
-    )
-
+        {'colors': {
+                    'red':'Красный',
+                    'orange':'Оранжевый',
+                    'yellow':'Желтый',
+                    'green': 'Зеленый',
+                    'aqua':'Голубой',
+                    'blue':'Синий',
+                    'violet':'Фиолетовый'}},
+                    )
